4558cdc1745bdc702db26bbe4f63e36fb8a58b50
Praveen <praveenmi187@gmail.com>
2026-08-31 17:54:28 +0530

    Changed the URL, Email ID and Password in config.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BASE_URL = 'https://www.amazon.com/'
+BASE_URL = 'https //www.amazon.in'
 
-email_id = os.environ.get('AMAZON_EMAIL', 'lmpravee+usca2@amazon.com')
-password_id = os.environ.get('AMAZON_PASSWORD', 'testing')
+email_id = os.environ.get('AMAZON_EMAIL', 'praveenmi187@amazon.com')
+password_id = os.environ.get('AMAZON_PASSWORD', 'Praveen_187')
 
