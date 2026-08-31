d37fce169bf2eee9550b650659696862c25880e7
Praveen <praveenmi187@gmail.com>
2026-08-31 17:59:05 +0530

    modified the URL link in config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_URL = 'https //www.amazon.in'
+BASE_URL = 'https://www.amazon.in'
 
 email_id = os.environ.get('AMAZON_EMAIL', 'praveenmi187@amazon.com')
 password_id = os.environ.get('AMAZON_PASSWORD', 'Praveen_187')
